8c2cd9f8fa0cc49b37ca3c95776ba2b9b0b500ad
Kacper <kacperrymkiewicz@gmail.com>
2024-12-05 21:59:13 +0100

    Add toastr library, global scss variables and header component

--- AI.Football.Predictions.Client/src/app/app.config.ts
+++ AI.Football.Predictions.Client/src/app/app.config.ts
@@ -6,6 +6,7 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { BrowserStorageService } from './core/services/browser-storage.service';
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -22,6 +23,12 @@ export const appConfig: ApplicationConfig = {
       // withInterceptors([httpInterceptor])
     ),
     provideAnimations(),
+    provideToastr({
+      timeOut: 4000,
+      positionClass: 'toast-top-right',
+      progressBar: true,
+      preventDuplicates: true
+    }), 
     BrowserStorageService
   ]
 };
--- AI.Football.Predictions.Client/src/app/shared/layout/header/header.component.ts
+++ AI.Football.Predictions.Client/src/app/shared/layout/header/header.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  private authService: AuthService = inject(AuthService);
+  private toastr: ToastrService = inject(ToastrService);
+  private router: Router = inject(Router);
+  // user?: UserDto | null;
+  role?: string | null;
+  isDropdownMenuOpen = signal(false);
 
+  constructor() {
+    // effect(() => {
+    //   this.user = this.authService.userData();
+    //   this.role = this.authService.userRole();
+    // });
+  }
+
+  onLogout() {
+    // this.authService.logoutUser();
+    // this.storageService.clearUserData();
+    // this.user = null;
+    this.router.navigate(['/login'])
+    this.toastr.success('Wylogowano pomyślnie!');
+  }
+
+  toggleDropdownMenu() {
+    this.isDropdownMenuOpen.set(!this.isDropdownMenuOpen());
+  }
 }
