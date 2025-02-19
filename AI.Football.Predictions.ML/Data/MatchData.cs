68db9ddc56a2fd6ee497cf9c1a2f9fbe2cc3efa2
Kacper <kacperrymkiewicz@gmail.com>
2025-02-19 18:39:49 +0100

    Add basic models for data predictions

---
+++ AI.Football.Predictions.ML/Data/MatchData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI.Football.Predictions.ML.Data
+{
+    public class MatchData
+    {
+        public float HomeGoalsAvg { get; set; }
+        public float AwayGoalsAvg { get; set; }
+        public float HomePossessionAvg { get; set; }
+        public float AwayPossessionAvg { get; set; }
+        public float HomeShotsAvg { get; set; }
+        public float AwayShotsAvg { get; set; }
+        public float HomeWinRate { get; set; }
+        public float AwayWinRate { get; set; }
+        public float H2HHomeWins { get; set; }
+        public float H2HAwayWins { get; set; }
+        public float H2HDraws { get; set; }
+    }
+}
---
+++ AI.Football.Predictions.ML/Data/MatchPrediction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI.Football.Predictions.ML.Data
+{
+    public class MatchPrediction
+    {
+        public float HomeWinProbability { get; set; }
+        public float DrawProbability { get; set; }
+        public float AwayWinProbability { get; set; }
+    }
+}
