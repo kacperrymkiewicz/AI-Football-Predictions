8e146769d19fe509542e9ac69e17b954d3c29f86
Kacper <kacperrymkiewicz@gmail.com>
2025-05-08 19:48:12 +0200

    Fetch matches by date, add custom style for datepicker

--- AI.Football.Predictions.Client/src/app/core/services/match.service.ts
+++ AI.Football.Predictions.Client/src/app/core/services/match.service.ts
@@ -14,7 +14,7 @@ export class MatchService {
 
   liveMatches = signal<SportradarResponse | null>(null);
 
-  public getLiveMatches(): Observable<SportradarResponse> {
+  public getLiveMatches() {
     this.loadingStateService.setState('liveMatches', true, false);
     return this.http.getLiveMatches().pipe(
       tap({
@@ -32,19 +32,39 @@ export class MatchService {
     )
   }
 
-  public getMatchDetails(matchId: number): Observable<SportradarMatchDetailsResponse> {
+  public getMatches(date: Date) {
+    this.loadingStateService.setState('liveMatches', true, false);
+    const endDate = new Date(date);
+    endDate.setDate(date.getDate() + 1);
+    return this.http.getMatches(date, endDate).pipe(
+      tap({
+        next: (matches) => {
+          this.liveMatches.set(matches);
+        },
+        error: (err) => {
+          this.loadingStateService.setState('liveMatches', false, true);
+          console.error('Błąd pobierania meczów:', err);
+        },
+        complete: () => {
+          this.loadingStateService.setState('liveMatches', false);
+        }
+      })
+    )
+  }
+
+  public getMatchDetails(matchId: number) {
     return this.http.getMatchDetails(matchId);
   }
 
-  public getMatchStatistics(matchId: number): Observable<SportradarMatchStatisticsResponse> {
+  public getMatchStatistics(matchId: number) {
     return this.http.getMatchStatistics(matchId);
   }
 
-  public getH2hMatches(matchId: number): Observable<SportradarHead2HeadResponse> {
+  public getH2hMatches(matchId: number) {
     return this.http.getH2hMatches(matchId);
   }
 
-  public getMatchPredictionById(matchId: number): Observable<MatchPrediction> {
+  public getMatchPredictionById(matchId: number) {
     return this.http.getMatchPredictionById(matchId);
   }
 }
--- AI.Football.Predictions.Client/src/app/features/live-matches/match-filter/match-filter.component.ts
+++ AI.Football.Predictions.Client/src/app/features/live-matches/match-filter/match-filter.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
@@ -7,6 +7,7 @@ import { FormsModule } from '@angular/forms';
 import { MatchDatePipe } from "../../../core/pipes/match-date.pipe";
 import { CustomDateAdapter } from './custom-date-adapter';
 import { RelativeDatePipe } from "../../../core/pipes/relative-date.pipe";
+import { MatchService } from '../../../core/services/match.service';
 
 @Component({
   selector: 'app-match-filter',
@@ -22,6 +23,7 @@ import { RelativeDatePipe } from "../../../core/pipes/relative-date.pipe";
   styleUrl: './match-filter.component.scss'
 })
 export class MatchFilterComponent {
+  protected matchService: MatchService = inject(MatchService);
   selectedDate: Date = new Date();
 
   changeDate(direction: string): void {
@@ -33,9 +35,12 @@ export class MatchFilterComponent {
       newDate.setDate(newDate.getDate() + 1);
     }
     this.selectedDate = newDate;
+    this.onDateChange();
   }
 
   onDateChange() {
-    
+    const correctedDate = new Date(this.selectedDate);
+    correctedDate.setHours(12, 0, 0, 0);
+    this.matchService.getMatches(correctedDate).subscribe();
   }
 }
